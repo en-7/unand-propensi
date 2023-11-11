e376b1242e1c6ba1cf3d778004ee771bd4de409f
assyifa.raudina7 <assyifa.raudina7@gmail.com>
2023-11-11 15:15:35 +0700

    resolve error?

--- sita/sita/src/main/java/protensi/sita/controller/UGBController.java
+++ sita/sita/src/main/java/protensi/sita/controller/UGBController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import protensi.sita.service.UgbServiceImpl;
 import protensi.sita.service.BaseService;
