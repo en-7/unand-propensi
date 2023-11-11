a9f6c0f87f19d33e479a3fa2fc02ac0a8054f031
daffa.aldin <daffa.aldin@gmail.com>
2023-11-11 12:53:12 +0700

    view all semhas

--- sita/sita/src/main/java/protensi/sita/controller/SeminarHasilController.java
+++ sita/sita/src/main/java/protensi/sita/controller/SeminarHasilController.java
@@ -1,9 +1,15 @@
 package protensi.sita.controller;
 
+import protensi.sita.model.UserModel;
+import protensi.sita.model.EnumRole;
+import protensi.sita.model.MahasiswaModel;
 import protensi.sita.model.SeminarHasilModel;
+import protensi.sita.model.UgbModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.UserDetailsAwareConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,36 +26,50 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import protensi.sita.service.SeminarProposalServiceImpl;
 import protensi.sita.service.UgbServiceImpl;
 import protensi.sita.service.MahasiswaServiceImpl;
-import protensi.sita.service.ManageUserService;
-import protensi.sita.service.ManageUserServiceImpl;
+import protensi.sita.security.UserDetailsServiceImpl;
 import protensi.sita.service.SeminarHasilServiceImpl;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
 public class SeminarHasilController {
 
     @Autowired
+    private UgbServiceImpl ugbService;
+
+    @Autowired
     private MahasiswaServiceImpl mahasiswaService;
 
     @Autowired
-    private ManageUserServiceImpl userService;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     private SeminarHasilServiceImpl seminarHasilService;
 
     @GetMapping("/seminar-hasil/add")
-    public String addSeminarHasilFormPage(Model model) {
-        SeminarHasilModel seminarHasilModel = new SeminarHasilModel();
+    public String addSeminarHasilFormPage(Model model, Authentication authentication) {
+        String namaUser = authentication.getName();
+        UserModel user = userDetailsService.findByUsername(namaUser);
+        if (user.getRoles().contains(EnumRole.MAHASISWA)) {
+            MahasiswaModel mahasiswa = mahasiswaService.findMahasiswaByUsername(user.getUsername());
+            UgbModel ugb = ugbService.findByIdMahasiswa(mahasiswa);
+            if (ugb.getStatusDokumen().equals("EVALUATED")) {
+                SeminarHasilModel seminarHasil = new SeminarHasilModel();
+                model.addAttribute("semhas", seminarHasil);
+                return "add-semhas-form";
+            } else {
+                return "error";
+            }
+        }
 
-        model.addAttribute("semhas", seminarHasilModel);
-
-        return "add-semhas-form";
+        else {
+            return "error";
+        }
     }
 
     @PostMapping("/seminar-hasil/add")
@@ -60,24 +80,48 @@ public class SeminarHasilController {
             @RequestParam("notes_sempro") MultipartFile notes_sempro,
             @RequestParam("form_saps") MultipartFile form_saps,
             @RequestParam("draft_TA") MultipartFile draft_TA,
-            Model model) throws IOException {
+            Model model, Authentication authentication) throws IOException {
         seminarHasil.setPersetujuanPembimbing(acc_pembimbing.getBytes());
         seminarHasil.setLaporanKP(bukti_kp.getBytes());
         seminarHasil.setRisalahSempro(risalah_sempro.getBytes());
         seminarHasil.setCatatanSempro(notes_sempro.getBytes());
         seminarHasil.setSaps(form_saps.getBytes());
         seminarHasil.setDraftLaporanTa(draft_TA.getBytes());
-        seminarHasil.setStatusDokumen("SUBMITTED");
-        seminarHasilService.addSeminarHasil(seminarHasil);
 
+        String namaUser = authentication.getName();
+        UserModel user = userDetailsService.findByUsername(namaUser);
+        MahasiswaModel mahasiswa = mahasiswaService.findMahasiswaById(user.getIdUser());
+        UgbModel ugb = ugbService.findByIdMahasiswa(mahasiswa);
+        seminarHasil.setUgb(ugb);
+
+        seminarHasil.getUgb().getMahasiswa().setTahap("SEMHAS");
+        seminarHasil.setStatusDokumen("SUBMITTED");
+
+        seminarHasilService.addSeminarHasil(seminarHasil);
+        model.addAttribute("semhas", seminarHasil);
         return "add-semhas-success";
     }
 
     @GetMapping("/seminar-hasil/viewall")
-    public String listSeminarHasil(Model model) {
-        List<SeminarHasilModel> listSeminarHasil = seminarHasilService.findAllSeminarHasil();
-        model.addAttribute("listSeminarHasil", listSeminarHasil);
-        return "viewall-semhas";
+    public String listSeminarHasil(Model model, Authentication authentication) {
+        String namaUser = authentication.getName();
+        UserModel user = userDetailsService.findByUsername(namaUser);
+
+        if (user.getRoles().contains(EnumRole.KOORDINATOR)) {
+            List<SeminarHasilModel> listSeminarHasil = seminarHasilService.findAllSeminarHasil();
+            model.addAttribute("listSeminarHasil", listSeminarHasil);
+            return "viewall-semhas";
+        } else if (user.getRoles().contains(EnumRole.PEMBIMBING) && user.getRoles().contains(EnumRole.PENGUJI)) {
+            List<SeminarHasilModel> listSemhasPembimbing = seminarHasilService.findAllByPembimbing(user.getIdUser());
+            List<SeminarHasilModel> listSemhasPenguji = seminarHasilService.findAllByPenguji(user.getIdUser());
+            List<SeminarHasilModel> listSemhas = new ArrayList<SeminarHasilModel>();
+            listSemhas.addAll(listSemhasPembimbing);
+            listSemhas.addAll(listSemhasPenguji);
+            model.addAttribute("listSemhas", listSemhas);
+            return "viewall-semhas-dosen";
+        } else {
+            return "error";
+        }
     }
 
 }
--- sita/sita/src/main/java/protensi/sita/service/SeminarHasilServiceImpl.java
+++ sita/sita/src/main/java/protensi/sita/service/SeminarHasilServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 import protensi.sita.model.SeminarHasilModel;
+import protensi.sita.model.SeminarProposalModel;
 import protensi.sita.repository.SeminarHasilDb;
 
 import java.util.List;
@@ -25,4 +26,11 @@ public class SeminarHasilServiceImpl {
         return seminarHasilDb.findAll();
     }
 
+    public List<SeminarHasilModel> findAllByPembimbing(Long pembimbingId) {
+        return seminarHasilDb.findAllByPembimbing(pembimbingId);
+    }
+
+    public List<SeminarHasilModel> findAllByPenguji(Long pengujiId) {
+        return seminarHasilDb.findAllByPenguji(pengujiId);
+    }
 }
