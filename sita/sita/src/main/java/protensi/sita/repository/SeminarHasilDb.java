21efb46ef134efbf6ac1ecab2974a1ae03993fa3
Daffa Aldin Wisnubroto <daffa.aldin@ui.ac.id>
2023-11-12 21:45:33 +0700

    SeminarHasil lagi

--- sita/sita/src/main/java/protensi/sita/repository/SeminarHasilDb.java
+++ sita/sita/src/main/java/protensi/sita/repository/SeminarHasilDb.java
@@ -1,9 +1,23 @@
 package protensi.sita.repository;
 
 import protensi.sita.model.SeminarHasilModel;
+import protensi.sita.model.SeminarProposalModel;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SeminarHasilDb extends JpaRepository<SeminarHasilModel, Long> {
+    Optional<SeminarHasilModel> findByIdSeminarHasil(Long idSeminarHasil);
 
+    List<SeminarHasilModel> findAllByStatusDokumen(String statusDokumen);
+
+    @Query("SELECT sh FROM SeminarHasilModel sh JOIN sh.ugb ugb JOIN ugb.pembimbing pembimbing WHERE pembimbing.id = :pembimbingId")
+    List<SeminarHasilModel> findAllByPembimbing(@Param("pembimbingId") Long pembimbingId);
+
+    @Query("SELECT sh FROM SeminarHasilModel sh JOIN sh.ugb ugb JOIN ugb.penguji penguji WHERE penguji.id = :pengujiId")
+    List<SeminarHasilModel> findAllByPenguji(@Param("pengujiId") Long pengujiId);
 }
